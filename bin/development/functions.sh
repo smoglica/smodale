d2e452b13ccb9c33971cdce5334e732fb837da55
Skender <skender.moglica@drivek.com>
2021-03-30 12:44:15 +0200

    chore: docker for development

---
+++ bin/development/functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IMAGE_NAME="smodale"
+TAG_NAME="development"
+
+docker_build() {
+  docker build \
+    -t $IMAGE_NAME:$TAG_NAME \
+    --build-arg USER_ID=$(id -u) \
+    --build-arg GROUP_ID=$(id -g) \
+    -f docker/$TAG_NAME/Dockerfile .
+}
+
+docker_run() {
+  docker run -it --rm -v $PWD:/usr/src/app $IMAGE_NAME:$TAG_NAME $*
+}

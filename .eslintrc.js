f636867793853dfb375aa3108e2b4d9ebaabbdaf
Skender <skender.moglica@drivek.com>
2021-03-30 16:07:31 +0200

    chore: installed eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: [
+    'svelte3',
+  ],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+};

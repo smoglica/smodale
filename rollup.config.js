d8a5bff117b3991bec55bac33af6582f41bdecdb
Skender <skender.moglica@drivek.com>
2021-04-01 09:23:11 +0200

    chore: plugin license

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
+import license from 'rollup-plugin-license';
 import pkg from './package.json';
 
 const { preprocess } = require('./svelte.config');
@@ -10,7 +11,7 @@ const name = pkg.name
   .replace(/^\w/, (m) => m.toUpperCase())
   .replace(/-\w/g, (m) => m[1].toUpperCase());
 
-export default {
+const config = {
   input: 'src/index.js',
   output: [
     { file: pkg.module, format: 'es', exports: 'named' },
@@ -21,5 +22,22 @@ export default {
       name,
     },
   ],
-  plugins: [svelte({ preprocess }), css({ output: 'smodale.css' }), resolve()],
+  plugins: [
+    svelte({ preprocess }),
+    css({ output: 'smodale.css' }),
+    resolve(),
+    license({
+      banner: {
+        commentStyle: 'ignored',
+        content: `
+          @package    <%= pkg.name %>
+          @author     <%= pkg.author.name %> <<%= pkg.author.email %>>
+          @version    <%= pkg.version %>
+          @build      <%= moment().format('LLLL') %>
+        `,
+      },
+    }),
+  ],
 };
+
+export default config;

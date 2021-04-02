5401191edf5ddde02d4cd73f38e66b02c55b8b09
Skender <skender.moglica@drivek.com>
2021-04-02 16:53:04 +0200

    chore: store initial values

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
       rules: {
         'import/no-mutable-exports': 'off',
         'import/prefer-default-export': 'off',
+        'import/first': 'off',
       },
     },
   ],

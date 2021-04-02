a563bc3e7eb98b541adf9e1cfbd48b4cf06a8b9c
Skender <skender.moglica@drivek.com>
2021-04-02 19:23:32 +0200

    chore: using codyframe

---
+++ demo/App.svelte
@@ -0,0 +1,14 @@
+<section class="margin-y-xl">
+  <div class="container max-width-sm">
+    <div class="margin-bottom-md text-component">
+      <h1>Smodale</h1>
+      <p class="text-md color-contrast-medium">
+        A simple, light and highly customizable modal for Svelte
+      </p>
+    </div>
+    <div class="flex flex-column flex-row@xs gap-xs">
+      <button type="button" class="btn btn--primary">Show a static modal</button>
+      <button type="button" class="btn btn--accent">Show a dynamic modal</button>
+    </div>
+  </div>
+</section>

c63af1e935b3f0fb90fbefb917436eecd148739e
Skender <skender.moglica@drivek.com>
2021-04-03 11:10:25 +0200

    chore: do not support ie11

--- demo/main.js
+++ demo/main.js
@@ -1,14 +1,3 @@
 import App from './App.svelte';
 
-if (!('CSS' in window) || !CSS.supports('color', 'var(--color-var)')) {
-  const cfStyle = document.getElementById('codyframe');
-
-  if (cfStyle) {
-    let href = cfStyle.getAttribute('href');
-
-    href = href.replace('style.css', 'style-fallback.css');
-    cfStyle.setAttribute('href', href);
-  }
-}
-
 export default new App({ target: document.body });

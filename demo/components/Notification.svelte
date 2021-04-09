12547e3e6547e215b750b4a26db5c1d0a00aad85
Skender <skender.moglica@drivek.com>
2021-04-09 11:45:04 +0200

    chore: notification slide transition

--- demo/components/Notification.svelte
+++ demo/components/Notification.svelte
@@ -1,10 +1,11 @@
 <script>
+  import { slide } from 'svelte/transition';
   import Alert from './Alert.svelte';
 
   const { notification = {}, onRemove = () => ({}) } = $$restProps;
 </script>
 
-<div class="margin-sm shadow-sm radius-md">
+<div class="margin-sm shadow-sm radius-md" role="status" aria-live="polite" in:slide out:slide>
   <Alert on:remove={onRemove} severity={notification?.type}>
     <div class="text-sm">
       {@html notification.text}

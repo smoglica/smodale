9890dcfe6d50ae2c507f1f3003ccc801ce5aabda
Skender <skender.moglica@drivek.com>
2021-04-11 15:14:33 +0200

    chore: starting modals component

--- src/components/Modals.svelte
+++ src/components/Modals.svelte
@@ -1,1 +1,12 @@
-<div />
+<script>
+  import Modal from './Modal.svelte';
+  import modals from '../store';
+</script>
+
+<div class="js-modals">
+  {#each $modals.dynamic as modal (modal.props.name)}
+    <Modal>
+      <svelte:component this={modal.component} {...modal.componentProps} />
+    </Modal>
+  {/each}
+</div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { writable, get } from 'svelte/store';
 import { error } from './logger';
+import Modals from './components/Modals.svelte';
 
 const store = writable({ static: [], dynamic: [] });
 const getState = () => get(store);
@@ -31,7 +32,41 @@ const showStaticModal = (name, resolve, reject) => {
     dynamic: modals?.dynamic,
   }));
 };
-const showDynamicModal = () => {};
+const showDynamicModal = (
+  component,
+  componentProps,
+  componentEvents,
+  props,
+  events,
+  resolve,
+  reject,
+) => {
+  const modalsElm = document.getElementsByClassName('js-modals')[0];
+
+  if (!modalsElm) {
+    // eslint-disable-next-line no-new
+    new Modals({ target: document.body });
+  }
+
+  store.update((modals) => ({
+    static: modals?.static,
+    dynamic: [
+      ...modals?.dynamic,
+      {
+        component,
+        componentProps,
+        componentEvents,
+        props: {
+          name: modals.length,
+          ...props,
+        },
+        events,
+        resolve,
+        reject,
+      },
+    ],
+  }));
+};
 
 const createModalsStore = () => ({
   ...store,
@@ -54,7 +89,7 @@ const createModalsStore = () => ({
       if (typeof name === 'string') {
         showStaticModal(name, resolve, reject);
       } else if (typeof name === 'function') {
-        showDynamicModal(...args);
+        showDynamicModal(...[args, resolve, reject]);
       } else {
         error('Invalid name or component');
       }

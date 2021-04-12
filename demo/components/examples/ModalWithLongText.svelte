b446b9c356d05d5692d6b211d7dfc311a0ddc242
Skender <skender.moglica@drivek.com>
2021-04-12 08:53:59 +0200

    chore: more modals

---
+++ demo/components/examples/ModalWithLongText.svelte
@@ -0,0 +1,93 @@
+<script>
+  import { getModalContext } from '../../../src';
+
+  const { hide, cancel } = getModalContext();
+</script>
+
+<div class="text-component">
+  <h2>Modal title</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+</div>
+<footer class="flex justify-end gap-xs margin-top-md">
+  <button type="button" class="btn btn--subtle" on:click={cancel}>Cancel</button>
+  <button type="button" class="btn btn--primary" on:click={hide}>Confirm</button>
+</footer>
---
+++ demo/components/examples/ModalWithTextOnly.svelte
@@ -0,0 +1,17 @@
+<div class="text-component">
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quasi iure tenetur doloremque
+    vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
+    reprehenderit reiciendis excepturi culpa!
+  </p>
+</div>

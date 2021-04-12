a5d72e3b233bbeedebe82b65b0fcf8a120c2f728
Skender <skender.moglica@drivek.com>
2021-04-12 11:15:00 +0200

    chore: using modals form icons

--- demo/components/examples/ModalWithCloseBtnInHeader.svelte
+++ demo/components/examples/ModalWithCloseBtnInHeader.svelte
@@ -1,4 +1,5 @@
 <script>
+  import eRemove from '../../assets/icons/e-remove.svg';
   import { getModalContext } from '../../../src';
 
   const { cancel } = getModalContext();
@@ -6,21 +7,8 @@
 
 <header class="flex items-center justify-between margin-bottom-sm">
   <h2>Modal title</h2>
-  <button class="reset inline-flex padding-xs js-tab-focus" type="button" on:click={cancel}>
-    <svg class="icon" viewBox="0 0 20 20">
-      <title>Close modal</title>
-      <g
-        fill="none"
-        stroke="currentColor"
-        stroke-miterlimit="10"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <line x1="3" y1="3" x2="17" y2="17" />
-        <line x1="17" y1="3" x2="3" y2="17" />
-      </g>
-    </svg>
+  <button class="btn btn--subtle padding-xs js-tab-focus" type="button" on:click={cancel}>
+    <img class="height-sm width-sm" src={eRemove} alt="Close" />
   </button>
 </header>
 <div class="text-component">

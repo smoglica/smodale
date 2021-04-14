a45c483714a659eecf42f2f5015a9ab7c1d064bd
Skender <skender.moglica@drivek.com>
2021-04-14 09:34:37 +0200

    chore: close button

--- demo/components/templates/SidebarLayout/SidebarLayout.svelte
+++ demo/components/templates/SidebarLayout/SidebarLayout.svelte
@@ -25,10 +25,9 @@
 <div class="padding-component hide@md no-js:is-hidden">
   <HamburgerMenuIcon
     ariaControls="sidebar"
-    class="btn btn--subtle radius-50% position-fixed top-0 right-0 z-index-fixed-element margin-top-sm margin-right-sm"
+    class="btn btn--subtle radius-50% position-fixed top-0 right-0 z-index-fixed-element margin-top-sm margin-right-md"
   />
 </div>
-
 <div class="flex@md">
   <aside
     class="sidebar sidebar--static@md sidebar--right-on-mobile js-sidebar"
@@ -39,13 +38,14 @@
   >
     <div class="sidebar__panel">
       <header class="sidebar__header z-index-2">
-        <h1 class="text-md text-truncate" id="sidebar-title">Sidebar title</h1>
-
-        <button class="reset sidebar__close-btn js-sidebar__close-btn js-tab-focus">
-          <img class="icon" src={eRemove} alt="Close panel" />
+        <div>
+          <h1 class="text-md text-truncate" id="sidebar-title">Smodale</h1>
+          <small class="color-contrast-medium">v1.0.0</small>
+        </div>
+        <button class="btn btn--subtle padding-sm js-sidebar__close-btn js-tab-focus" type="button">
+          <img class="icon icon--sm" src={eRemove} alt="Close panel" />
         </button>
       </header>
-
       <div class="position-relative z-index-1">
         <div class="padding-md">
           <slot name="aside" />
@@ -53,8 +53,7 @@
       </div>
     </div>
   </aside>
-
-  <main class="position-relative z-index-1 flex-grow height-100vh">
+  <main class="position-relative flex-grow height-100vh">
     <div class="padding-md">
       <slot />
     </div>

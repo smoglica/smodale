6a4adab96b4a7b5c36e919d6cd9e938df7d777f6
Skender <skender.moglica@drivek.com>
2021-04-16 11:40:35 +0200

    chore: normalize white space

--- demo/components/molecules/CodeSnippet/CodeSnippet.svelte
+++ demo/components/molecules/CodeSnippet/CodeSnippet.svelte
@@ -6,27 +6,30 @@
 </script>
 
 <script>
+  import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
+
   export let language = 'javascript';
   export let filename = '';
   export let code = '';
 
-  const onMount = (node) => {
-    window.Prism.highlightElement(node);
-  };
+  const onMount = (node) => window.Prism.highlightElement(node);
 </script>
 
-<div class="code-snippet position-relative">
-  {#if filename}
-    <div class="position-absolute top-0 right-0 text-xs margin-top-sm margin-right-md font-mono">
-      {filename}
-    </div>
-  {/if}
-  <pre
-    class="radius-md">
+{#if code}
+  <div class="code-snippet position-relative">
+    {#if filename}
+      <div class="position-absolute top-0 right-0 text-xs margin-top-sm margin-right-md font-mono">
+        {filename}
+      </div>
+    {/if}
+    <pre
+      class="radius-md"
+      {...$$restProps}>
     <code class="language-{language}" use:onMount>
       {@html code}
     </code>
   </pre>
-</div>
+  </div>
+{/if}
 
 <style src="_index.scss"></style>

e9e0a67776517ea3c09392f7733d31d033a01bc9
Skender Moglica <moglica.s@gmail.com>
2021-04-26 20:21:41 +0200

    chore: removed examples folder

--- demo/components/organisms/SectionLongContent/SectionLongContent.svelte
+++ demo/components/organisms/SectionLongContent/SectionLongContent.svelte
@@ -1,12 +1,10 @@
 <script>
-  import LongContentExample from 'Demo/components/organisms/SectionLongContent/examples/LongContentExample';
+  import LongContentExample from 'Demo/components/organisms/SectionLongContent/LongContentExample';
   import Details from 'Demo/components/molecules/Details';
   import CodeSnippet from 'Demo/components/molecules/CodeSnippet';
 </script>
 
 <LongContentExample />
 <Details class="margin-top-sm" title="Show code">
-  <CodeSnippet
-    data-src="demo/components/organisms/SectionLongContent/examples/LongContentExample.svelte"
-  />
+  <CodeSnippet data-src="demo/components/organisms/SectionLongContent/LongContentExample.svelte" />
 </Details>

a53ef7e104a0f270d0b5aea77af603c3686df150
Skender Moglica <moglica.s@gmail.com>
2021-04-27 08:31:58 +0200

    chore: additional nested modal

--- demo/components/examples/ModalWithTextOnly.svelte
+++ demo/components/examples/ModalWithTextOnly.svelte
@@ -1,8 +1,27 @@
 <script>
-  import { getModalContext } from 'smodale';
+  import smodale, { getModalContext } from 'smodale';
   import arrowLeft from 'Demo/assets/icons/arrow-left.svg';
+  import ModalWithButtons from 'Demo/components/examples/ModalWithButtons';
 
   const { cancel } = getModalContext();
+
+  const onClick = () =>
+    smodale.show(ModalWithButtons, null, {
+      padding: 'var(--component-padding)',
+      backdropColor: 'transparent',
+      breakpoints: {
+        '32rem': {
+          borderRadius: 'var(--radius-md)',
+          margin: 'var(--component-padding)',
+          maxWidth: 'var(--max-width-xxs)',
+          centered: true,
+          scrollable: true,
+        },
+        '48rem': {
+          maxWidth: 'var(--max-width-sm)',
+        },
+      },
+    });
 </script>
 
 <header class="margin-bottom-sm">
@@ -27,4 +46,5 @@
     vitae cumque sint iusto, optio laborum ut dolore sequi blanditiis, accusamus qui officiis
     reprehenderit reiciendis excepturi culpa!
   </p>
+  <button class="btn btn--primary" type="button" on:click={onClick}>Show nested modal</button>
 </div>

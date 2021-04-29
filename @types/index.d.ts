12aa4f02de68a1ce59429ce01de9b4235c140b7f
Skender <moglica.s@gmail.com>
2021-04-29 20:16:05 +0200

    chore: enhanced types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,16 +1,59 @@
 declare module 'smodale' {
   import { SvelteComponent } from 'svelte/types/runtime';
+  import { TransitionConfig } from 'svelte/types/runtime/transition';
 
-  export function getModalContext();
-  export let ModalDismissReasons;
+  interface ModalContext {
+    hide(data?: any): void;
+    cancel(data?: any): void;
+    component: SvelteComponent;
+  }
+
+  interface ModalEvents {
+    opened(): void;
+    closed(): void;
+  }
+  interface Breakpoints {
+    maxWidth?: string;
+    margin?: string;
+    height?: string;
+    backgroundColor?: string;
+    padding?: string;
+    borderRadius?: string | number;
+    backdropColor?: string;
+    boxShadow?: string;
+    centered?: boolean;
+    scrollable?: boolean;
+    zIndex?: string | number;
+  }
+
+  interface ModalProps extends Breakpoints {
+    name: string;
+    transition?: (node: Element, params: any) => TransitionConfig;
+    transitionParams: TransitionConfig;
+    escapeToClose?: boolean;
+    clickOutsideToClose?: boolean;
+    disableBodyScroll?: boolean;
+    focusOnOpen?: boolean;
+    focusTrap?: boolean;
+    classes?: string;
+    breakpoints?: { [breakpoint: string]: Breakpoints };
+  }
+
+  type ModalDismissReasons = {
+    BACKDROP_CLICK: string;
+    ESC: string;
+  };
+
+  export function getModalContext(): ModalContext;
+  export let ModalDismissReasons: ModalDismissReasons;
   export let Modal: SvelteComponent;
 
   export default {
     show(
       name: string | SvelteComponent,
-      componentProps?: object,
-      modalProps?: object,
-      modalEvents?: object
+      componentProps?: {},
+      modalProps?: ModalProps,
+      modalEvents?: ModalEvents
     ): Promise<any>;,
     hide(name: string, data: any): void;,
     cancel(name: string, data: any): void;,

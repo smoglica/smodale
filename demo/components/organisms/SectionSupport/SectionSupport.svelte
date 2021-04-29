10d5f244398f0aac2fdc38e3a169a7977cce230a
Skender <moglica.s@gmail.com>
2021-04-29 14:38:04 +0200

    chore: support improved

--- demo/components/organisms/SectionSupport/SectionSupport.svelte
+++ demo/components/organisms/SectionSupport/SectionSupport.svelte
@@ -1,6 +1,13 @@
 <p>
   Smodale module targets modern browsers. However if you want to support older browsers make sure to
-  transpile <code>ECMAScript 2020</code> and provide
+  <a
+    target="_blank"
+    ref="noopener noreferrer"
+    href="https://svelte-recipes.netlify.app/build-setup/#transpiling-es6-to-es5-for-legacy-browser-ie11-support-with-babel"
+  >
+    transpile
+  </a>
+  <code>ECMAScript 2020</code> and provide
   <a
     target="_blank"
     ref="noopener noreferrer"
